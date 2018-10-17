9fcbad3120e5d48ec7645633683d1628f8e01128
James Nagle <James.Nagle@students.ittralee.ie>
2018-10-17 14:44:37 +0100

    created login

--- src/BowlingDriver.java
+++ src/BowlingDriver.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class BowlingDriver extends JFrame{
 
-    private JPanel mainDriverPanel;
+    private JPanel mainDriverPanel, panel, loginPanel, loginPage, homePanel;
     private CardLayout cardLayout;
     private static final int frameWidth = 310;
     private static final int frameHeight = 200;
@@ -19,9 +19,67 @@ public class BowlingDriver extends JFrame{
         //Package bdd = new ChildBirthdayPackage(10);
     }
 
-    public BowlingDriver(){
+    public BowlingDriver() {
+
+        setTitle("Bowling System");
+        setSize(frameWidth, frameHeight);
+
         Container cont = getContentPane();
+
+        mainDriverPanel = new JPanel(new CardLayout());
+        cardLayout = (CardLayout) (mainDriverPanel.getLayout());
+
+        loginPanel = createLoginPanel();
+        mainDriverPanel.add(loginPanel, "Login");
+        cardLayout.show(mainDriverPanel, "Login");
+
+        loginPage = createLogin();
+        mainDriverPanel.add(loginPage, "Login Page");
+
+        cont.add(mainDriverPanel);
     }
+
+
+
+        public JPanel createLoginPanel(){
+            panel = new JPanel(new GridLayout(0,1));
+            JButton login = new JButton("Login");
+            login.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    cardLayout.show(mainDriverPanel, "Login Page");
+                }
+            });
+            panel.add(login);
+            return panel;
+    }
+
+    public JPanel createLogin(){
+        panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel lbl_login = new JLabel("Login");
+        JTextField txtfld_login = new JTextField(20);
+        JButton btn_login = new JButton("Login");
+        btn_login.addActionListener(new ActionListener() {
+                                        //loads player details from arraylist
+                                        public void actionPerformed(ActionEvent e) {
+                                            homePanel = createHomePanel();
+                                            mainDriverPanel.add(homePanel, "Home");
+                                            cardLayout.show(mainDriverPanel, "Home");
+                                        }
+                                    });
+        panel.add(lbl_login);
+        panel.add(txtfld_login);
+        panel.add(btn_login);
+        return panel;
+    }
+
+    public JPanel createHomePanel(){
+        panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel lbl_login = new JLabel("Home");
+        panel.add(lbl_login);
+        return panel;
+    }
+
+
 
 
 }
